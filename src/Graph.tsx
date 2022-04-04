be628e5c17b84b6b19887a7a1f50c7f9c0dd57d6
Miguel Ferrer <mferrer.ieu2021@student.ie.edu>
2022-04-05 01:40:53 +0200

    enter

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -52,8 +52,12 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view' , 'y_line'),
       elem.setAttribute('column-pivots' , '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('collumns', '["top_ask_price"]');
-      elem.setAttribute('aggregates' , `{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}`);
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates' ,
+       `{"stock":"distinct count",
+       "top_ask_price":"avg",
+       "top_bid_price":"avg",
+       "timestamp":"distinct count"}`);
     }
   }
   //At this point graph should appear. Done.
